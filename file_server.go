c19c9febd02a7c312b17dd5db16c28e893e1a6f1
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2020-05-27 22:42:35 +0300

    wrapping errors

--- file_server.go
+++ file_server.go
@@ -32,10 +32,14 @@ func (fs *fileServer) makePath(storage string, isPermanent bool, file string) st
 	return path.Join(storage, file)
 }
 
+func wrapError(context, description string, err error) error {
+	return fmt.Errorf("[%s] %s: %w", context, description, err)
+}
+
 func (fs *fileServer) List(ctx context.Context, req *proto.ListRequest, rsp *proto.ListResponse) error {
 	fi, err := fs.storage.Files(fs.makePath(req.GetStorage(), req.GetIsPermanent(), ""))
 	if err != nil {
-		return err
+		return wrapError("List", "get files error", err)
 	}
 
 	files := make([]*proto.File, 0, len(fi))
@@ -54,7 +58,7 @@ func (fs *fileServer) List(ctx context.Context, req *proto.ListRequest, rsp *pro
 func (fs *fileServer) GetFile(ctx context.Context, req *proto.FileRequest, stream proto.FileService_GetFileStream) error {
 	file, err := fs.storage.File(fs.makePath(req.GetStorage(), req.GetIsPermanent(), ""), req.GetFileName())
 	if err != nil {
-		return err
+		return wrapError("GetFile", "get file error", err)
 	}
 	defer file.Close()
 
@@ -70,7 +74,7 @@ func (fs *fileServer) GetFile(ctx context.Context, req *proto.FileRequest, strea
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			return err
+			return wrapError("GetFile", "send data error", err)
 		}
 	}
 	return nil
@@ -111,7 +115,7 @@ func metadataFromUploadRequest(stream proto.FileService_UploadFileStream) (*prot
 func (fs *fileServer) UploadFile(ctx context.Context, stream proto.FileService_UploadFileStream) error {
 	metadata, err := metadataFromUploadRequest(stream)
 	if err != nil {
-		return fmt.Errorf("[UploadFile] metadata error: %w", err)
+		return wrapError("UploadFile", "metadata error", err)
 	}
 
 	r, w := io.Pipe()
@@ -130,7 +134,7 @@ func (fs *fileServer) UploadFile(ctx context.Context, stream proto.FileService_U
 		}
 
 		if closeError != nil {
-			w.CloseWithError(fmt.Errorf("[UploadFile] write error: %w", closeError))
+			w.CloseWithError(wrapError("UploadFile", "write error", closeError))
 			return
 		}
 
@@ -139,57 +143,61 @@ func (fs *fileServer) UploadFile(ctx context.Context, stream proto.FileService_U
 
 	file, err := fs.tempStorage.Store("", metadata.GetFileName(), r)
 	if err != nil {
-		return fmt.Errorf("[UploadFile] store error: %w", err)
+		return wrapError("UploadFile", "store error", err)
 	}
 
 	dir := fs.makePath(metadata.GetStorage(), metadata.GetIsPermanent(), "")
 	err = fs.storage.Move(file, dir, metadata.GetFileName())
 	if err != nil {
 		file.Remove()
-		return fmt.Errorf("[UploadFile] move error: %w", err)
+		return wrapError("UploadFile", "move error", err)
 	}
 
-	stream.Send(&proto.File{
+	err = stream.Send(&proto.File{
 		Name:    file.Name(),
 		Size:    0,
 		ModTime: 0,
 	})
+
+	if err != nil {
+		return wrapError("UploadFile", "send file response error", err)
+	}
+
 	return nil
 }
 
 func (fs *fileServer) RemoveFile(ctx context.Context, req *proto.FileRequest, rsp *proto.EmptyResponse) error {
 	err := fs.storage.Remove(fs.makePath(req.GetStorage(), req.GetIsPermanent(), ""), req.GetFileName())
-	rsp = &proto.EmptyResponse{}
-	return err
+	if err != nil {
+		return wrapError("RemoveFile", "remove file error", err)
+	}
+	return nil
 }
 
 func (fs *fileServer) CreateStorage(ctx context.Context, req *proto.CreateStorageRequest, rsp *proto.CreateStorageResponse) error {
 	status := proto.StorageStatus_Ok
 	sPath := fs.makePath(req.GetName(), false, "")
+
 	err := fs.storage.Mkdir(sPath)
 	if errors.Is(err, filesystem.ErrAlreadyExist) {
 		status = proto.StorageStatus_AlreadyExist
 	} else if err != nil {
-		return err
+		return wrapError("CreateStorage", "create folder error", err)
 	}
 
 	if req.GetWithPermanent() {
 		err = fs.storage.Mkdir(fs.makePath(req.GetName(), true, ""))
 		if err != nil {
 			fs.storage.RemoveDir(sPath)
-			return err
+			return wrapError("CreateStorage", "create permanent folder error", err)
 		}
 	}
 
-	rsp = &proto.CreateStorageResponse{
-		Status: status,
-	}
+	rsp.Status = status
 	return nil
 }
 
 func (fs *fileServer) IsStorageExists(ctx context.Context, req *proto.IsStorageExistsRequest, rsp *proto.BoolResponse) error {
-	rsp = &proto.BoolResponse{
-		Flag: fs.storage.IsExist(req.GetName()),
-	}
+	rsp.Flag = fs.storage.IsExist(req.GetName())
 	return nil
 }
